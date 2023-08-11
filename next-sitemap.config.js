c4f032b1260ad8c92a09c4f964862f9e7f11582c
Jonghyun <rhrhdksk@gmail.com>
2023-08-12 00:04:30 +0900

    test

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: "https://www.xdeal.site/",
-	changefreq: "daily",
-	priority: 0.7,
-	sitemapSize: 7000,
+	siteUrl: "hhttps://www.xdeal.site/",
 	generateRobotsTxt: true,
-	exclude: [],
+	sitemapSize: 7000,
 }
