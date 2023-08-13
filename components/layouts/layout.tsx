8aee56cd230eca886272ad40000754ae4249bad7
Jonghyun <rhrhdksk@gmail.com>
2023-08-13 15:04:52 +0900

    SEO

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -34,9 +34,9 @@ const LayOut = ({ children, meta, loading = false }: LayOutProps) => {
     return (
         <>
             <Head>
-                <title>{meta?.title ?? `X-DEAL :: 최저가 상품 추천`}</title>
-                <meta name="description" content={meta?.description ?? `저렴한 가격의 제품을 추천하는 다른 쇼핑몰 비교 웹사이트입니다. 다양한 제품 카테고리에서 가장 저렴한 옵션을 찾아보세요. 최고의 거래를 확인하고 비용을 절약하세요!`} />
-                <meta name="keywords" content={meta?.keywords ?? `쇼핑몰 추천, 저렴한 상품, 가격 비교, 할인 상품, 비용 절약, 쇼핑 가이드`} />
+                <title>{meta?.title ?? `최저가 상품 추천앱, 엑스딜::X-DEAL`}</title>
+                <meta name="description" content={meta?.description ?? `저렴한 가격의 제품을 추천하는 다른 쇼핑몰 비교 웹사이트입니다. 다양한 제품 카테고리에서 가장 저렴한 옵션을 찾아보세요.`} />
+                <meta name="keywords" content={meta?.keywords ?? `엑스딜,쇼핑몰 추천, 저렴한 상품, 가격 비교, 할인 상품, 비용 절약, 쇼핑 가이드`} />
                 <meta name="copyright" content="Copyright © 2023 by X-Deal" />
                 <meta name="robots" content="index, follow" />
                 <meta name="language" content="한국어" />
@@ -44,9 +44,9 @@ const LayOut = ({ children, meta, loading = false }: LayOutProps) => {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="naver-site-verification" content="831643bd3a4a9ef983886171be497fd96764c34b" />
                 <meta property="og:type" content="website" />
-                <meta property="og:site_name" content="엑스딜(Xdeal)" />
-                <meta property="og:title" content={meta?.ogTitle ?? `🔥 엑스딜(Xdeal) - 최저가 상품 추천으로 신나는 쇼핑 여정 시작하기! 🔥`} />
-                <meta property="og:description" content={meta?.ogDescription ?? `저렴한 가격의 제품을 추천하는 다른 쇼핑몰 비교 웹사이트입니다. 다양한 제품 카테고리에서 가장 저렴한 옵션을 찾아보세요. 최고의 거래를 확인하고 비용을 절약하세요!`} />
+                <meta property="og:site_name" content="최저가 상품 추천앱, 엑스딜::X-DEAL" />
+                <meta property="og:title" content={meta?.ogTitle ?? `최저가 상품 추천앱, 엑스딜::X-DEAL`} />
+                <meta property="og:description" content={meta?.ogDescription ?? `저렴한 가격의 제품을 추천하는 다른 쇼핑몰 비교 웹사이트입니다. 다양한 제품 카테고리에서 가장 저렴한 옵션을 찾아보세요.`} />
                 <meta property="og:image" content={meta?.ogImage ?? `/favicon/og.png`} />
 
             </Head>
