a3f954fad77aa4e0e408577c874e905308de2b43
Jonghyun <rhrhdksk@gmail.com>
2023-08-15 17:37:39 +0900

    coupang

--- src/pages/api/v1/product/coupang/count.ts
+++ src/pages/api/v1/product/coupang/count.ts
@@ -8,13 +8,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === "GET") {
 		const { category } = req.query
 		if (!category || category === "전체") {
-			const query = "SELECT COUNT(*) as total FROM coupangItem_memory"
+			const query = "SELECT COUNT(*) as total FROM coupangItem"
 			const result = await mariaDB.query(query)
 			const total = Math.ceil(result[0].total / 16)
 			res.status(200).json(total)
 		} else {
 			const countQuery =
-				"SELECT COUNT(*) as total FROM coupangItem_memory WHERE category = ?;"
+				"SELECT COUNT(*) as total FROM coupangItem WHERE category = ?;"
 			const query = await mariaDB.query<any>(countQuery, [category])
 			const total = Math.ceil(query[0].total / 16)
 			res.status(200).json(total)
